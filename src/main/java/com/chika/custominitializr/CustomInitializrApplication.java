f3e9f8005a89f7b6b49452523168e5c71a5a8c04
takumi34 <1shiranui@gmail.com>
2019-11-14 19:26:35 +0900

    first

---
+++ src/main/java/com/chika/custominitializr/CustomInitializrApplication.java
@@ -0,0 +1,13 @@
+package com.chika.custominitializr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomInitializrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomInitializrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chika/custominitializr/CustomInitializrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chika.custominitializr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomInitializrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
